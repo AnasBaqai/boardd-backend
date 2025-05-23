3e150f97c768377d58263d37ed09b5e876175504
AnasBaqai <anashabib139@gmail.com>
2025-05-23 02:10:06 +0200

    create project

---
+++ controller/projectController.js
@@ -0,0 +1,57 @@
+const { findChannel } = require("../models/channelModel");
+const { findChannelTab } = require("../models/channelTabsModel");
+const { parseBody, generateResponse, formatDate } = require("../utils");
+const { STATUS_CODES } = require("../utils/constants");
+
+exports.createProject = async (req, res, next) => {
+  try {
+    const {
+      name,
+      description,
+      tabId,
+      status,
+      startDate,
+      endDate,
+      color,
+      priority,
+    } = parseBody(req.body);
+    const userId = req?.user?.id;
+
+    // check if tabId is valid
+    const tab = await findChannelTab({ _id: tabId });
+    if (!tab) {
+      return generateResponse(
+        null,
+        "Tab not found",
+        res,
+        STATUS_CODES.NOT_FOUND
+      );
+    }
+    const companyId = tab?.companyId;
+    const channelId = tab?.channelId;
+
+    // create project
+    const project = await createProject({
+      name,
+      description,
+      createdBy: userId,
+      companyId,
+      channelId,
+      tabId,
+      status,
+      startDate: formatDate(startDate),
+      endDate: formatDate(endDate),
+      color,
+      priority,
+    });
+
+    return generateResponse(
+      project,
+      "Project created successfully",
+      res,
+      STATUS_CODES.SUCCESS
+    );
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ models/projectModel.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const { Schema, model, Types } = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+const { getMongooseAggregatePaginatedData } = require("../utils");
+
+const projectSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  createdBy: { type: Types.ObjectId, ref: "User", required: true },
+  companyId: { type: Types.ObjectId, ref: "Company", required: true },
+  channelId: { type: Types.ObjectId, ref: "Channel", required: true },
+  tabId: { type: Types.ObjectId, ref: "ChannelTab", required: true },
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+  color: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+// register pagination plugin to project model
+projectSchema.plugin(mongoosePaginate);
+projectSchema.plugin(aggregatePaginate);
+
+const ProjectModel = model("Project", projectSchema);
+
+// create new project
+exports.createProject = (obj) => ProjectModel.create(obj);
+
+// find project by query
+exports.findProject = (query) => ProjectModel.findOne(query);
+
+// get all projects
+exports.getAllProjects = async ({
+  query,
+  page,
+  limit,
+  responseKey = "data",
+}) => {
+  const { data, pagination } = await getMongooseAggregatePaginatedData({
+    model: ProjectModel,
+    query,
+    page,
+    limit,
+  });
+  return { [responseKey]: data, pagination };
+};
